bc6b7ebf77328150916627218a7c35687b46c8c3
Vishakha Sawra <vishakhasawra@gmail.com>
2024-07-06 14:02:44 +0530

    ami id

--- web/main.tf
+++ web/main.tf
@@ -1,6 +1,6 @@
 # EC2 instance
 resource "aws_instance" "server" {
-    ami             = "ami-06c68f701d8090592"  
+    ami             = "ami-id"  
     instance_type   = "t2.micro"
     subnet_id       = var.subnet_id
     security_groups = [var.security_group_id]
